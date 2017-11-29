3590555a5d67c4996c9a892f04d68b66cfc4e085
Raoul Kalisvaart <R.D.Kalisvaart@student.tudelft.nl>
2017-11-29 23:08:09 +0100

    added an add method

--- js/script.js
+++ js/script.js
@@ -16,15 +16,32 @@ Habit.prototype.setName = function(name) { this.name = name };
 Habit.prototype.setDescription = function(description) { this.description = description }
 Habit.prototype.setDays = function(days) { this.days = days };
 
-
-
-
 var MainModule = ( function () {
 
-	var habitCatalog = new HabitCatalog();
+    var habitCatalog = new HabitCatalog();
 
 	return {
+        addElement: function(){
+            var ul = document.getElementById('habitList');
+            var il = document.createElement('li');
+            var userInput = document.getElementById('userInputHabit').value
+            var userDescription = document.getElementById('userInputDesc').value;
+            var habitDays = [];
 
+            //var inputs = document.getElementsByTagName('input').getElementsByTagName("input")
+
+           /* for(var i = 0, max = inputs.length; i < max; i+= 1){
+                if(inputs[i].type === "checkbox" && inputs[i].checked) {
+                    habitDays.push(inputs[i].value);
+                 }
+        }*/
+            var newHabit = new Habit(userInput, userDescription, null, null);
+        }
 	}
 
 })();
+
+window.onload = function(){
+    document.getElementById("habitNew").onclick = MainModule.addElement;
+    //document.getElementById("habitDelete").onclick = deleteElement;
+}
