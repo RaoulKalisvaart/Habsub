78e69202d89f79aa4e63c71283f4951e8f3483a4
Raoul Kalisvaart <R.D.Kalisvaart@student.tudelft.nl>
2017-12-21 13:17:11 +0100

    Added CSS

--- client/js/script.js
+++ client/js/script.js
@@ -141,7 +141,90 @@ var MainModule = ( function () {
         },
     
         showElements: function(){
-            var tempArray = habitCatalog.getHabits();
+			var tempArray = habitCatalog.getHabits();
+			var table = document.getElementById("habits");
+			var nRows = table.rows.length;
+			//for(var i = 1; i < nRows; i += 1){
+				//table.deleteRow(i);
+			//}
+
+			while(table.rows.length > 1)
+			{
+				table.deleteRow(1);
+			}
+
+			for(var i = 0, max = tempArray.length; i < max; i += 1){
+				
+				var row = table.insertRow(1);
+				var cell1 = row.insertCell(0);
+				var cell2 = row.insertCell(1);
+				var cell3 = row.insertCell(2);
+				var cell4 = row.insertCell(3);
+				var cell5 = row.insertCell(4);
+				var cell6 = row.insertCell(5);
+				var cell7 = row.insertCell(6);
+				var cell8 = row.insertCell(7);
+
+				cell1.innerHTML = tempArray[i].getName();
+				if(tempArray[i].getDays().indexOf("Monday") !== -1){
+					var output = ""
+					output += "X" + "<button class=\"deleteButton\" onclick=\"MainModule.deleteHabit(" + i + ")\">delete</button>\n";
+					output += "<button class=\"editButton\" onclick=\"MainModule.setToEdit(" + i + ")\">edit</button>\n";
+					output += "<button class=\"doneButton\" onclick=\"MainModule.setDone(" + i + ")\">done!</button>\n";
+					cell2.innerHTML = output;
+				}
+				if(tempArray[i].getDays().indexOf("Tuesday") !== -1){
+					var output = ""
+					output += "X" + "<button class=\"deleteButton\" onclick=\"MainModule.deleteHabit(" + i + ")\">delete</button>\n";
+					output += "<button class=\"editButton\" onclick=\"MainModule.setToEdit(" + i + ")\">edit</button>\n";
+					output += "<button class=\"doneButton\" onclick=\"MainModule.setDone(" + i + ")\">done!</button>\n";
+					cell3.innerHTML = output;
+				}
+				if(tempArray[i].getDays().indexOf("Wednesday") !== -1){
+					var output = ""
+					output += "X" + "<button class=\"deleteButton\" onclick=\"MainModule.deleteHabit(" + i + ")\">delete</button>\n";
+					output += "<button class=\"editButton\" onclick=\"MainModule.setToEdit(" + i + ")\">edit</button>\n";
+					output += "<button class=\"doneButton\" onclick=\"MainModule.setDone(" + i + ")\">done!</button>\n";
+					cell4.innerHTML = output;
+				}
+				if(tempArray[i].getDays().indexOf("Thursday") !== -1){
+					var output = ""
+					output += "X" + "<button class=\"deleteButton\" onclick=\"MainModule.deleteHabit(" + i + ")\">delete</button>\n";
+					output += "<button class=\"editButton\" onclick=\"MainModule.setToEdit(" + i + ")\">edit</button>\n";
+					output += "<button class=\"doneButton\" onclick=\"MainModule.setDone(" + i + ")\">done!</button>\n";
+					cell5.innerHTML = output;
+				}
+				if(tempArray[i].getDays().indexOf("Friday") !== -1){
+					var output = ""
+					output += "X" + "<button class=\"deleteButton\" onclick=\"MainModule.deleteHabit(" + i + ")\">delete</button>\n";
+					output += "<button class=\"editButton\" onclick=\"MainModule.setToEdit(" + i + ")\">edit</button>\n";
+					output += "<button class=\"doneButton\" onclick=\"MainModule.setDone(" + i + ")\">done!</button>\n";
+					cell6.innerHTML = output;
+				}
+				if(tempArray[i].getDays().indexOf("Saturday") !== -1){
+					var output = ""
+					output += "X" + "<button class=\"deleteButton\" onclick=\"MainModule.deleteHabit(" + i + ")\">delete</button>\n";
+					output += "<button class=\"editButton\" onclick=\"MainModule.setToEdit(" + i + ")\">edit</button>\n";
+					output += "<button class=\"doneButton\" onclick=\"MainModule.setDone(" + i + ")\">done!</button>\n";
+					cell7.innerHTML = output;
+				}
+				if(tempArray[i].getDays().indexOf("Sunday") !== -1){
+					var output = ""
+					output += "X" + "<button class=\"deleteButton\" onclick=\"MainModule.deleteHabit(" + i + ")\">delete</button>\n";
+					output += "<button class=\"editButton\" onclick=\"MainModule.setToEdit(" + i + ")\">edit</button>\n";
+					output += "<button class=\"doneButton\" onclick=\"MainModule.setDone(" + i + ")\">done!</button>\n";
+					cell8.innerHTML = output;
+				}
+
+				if(tempArray[i].getMood() == "Positive"){
+					row.style.backgroundColor = "LightGreen";
+				}
+				if(tempArray[i].getMood() == "Negative"){
+					row.style.backgroundColor = "LightCoral";
+				}
+			}
+			
+			/*
             var ul = document.getElementById('habitList');
 
             while (ul.firstChild) {
@@ -149,6 +232,7 @@ var MainModule = ( function () {
 			}
 
             for(var i = 0, max = tempArray.length; i < max; i += 1){
+
                 var output;
                 output = "Habit: " + tempArray[i].getName() + "; Description: " + tempArray[i].getDescription() + "; Days:";
                 for(var j = 0; j < tempArray[i].getDays().length; j++)
@@ -179,7 +263,7 @@ var MainModule = ( function () {
                 var il = document.createElement('li');
                 il.innerHTML = output;
                 ul.appendChild(il);
-            }
+            }*/
         },
 
         editHabit: function (form) {
